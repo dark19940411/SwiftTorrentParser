82726850a2a513649e5bc7bd5811ec0f361a3352
dark19940411 <279714010@qq.com>
2017-08-14 11:07:49 +0800

    Initial Commit

---
+++ SwiftTorrentParser/SwiftTorrentParser.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTorrentParser.h
+//  SwiftTorrentParser
+//
+//  Created by turtle on 2017/8/14.
+//  Copyright © 2017年 turtle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTorrentParser.
+FOUNDATION_EXPORT double SwiftTorrentParserVersionNumber;
+
+//! Project version string for SwiftTorrentParser.
+FOUNDATION_EXPORT const unsigned char SwiftTorrentParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTorrentParser/PublicHeader.h>
+
+
